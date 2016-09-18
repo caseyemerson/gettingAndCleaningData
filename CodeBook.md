c560ef4b2217b4483bc0d2ba2fecf02ce49cb8dc
Casey <Casey@caseyemerson.org>
2016-09-18 18:42:49 -0400

    corrected CodeBook.md

--- CodeBook.md
+++ CodeBook.md
@@ -17,32 +17,32 @@ These signals were used to estimate variables of the feature vector for each pat
 ## Original Data Set Contents
 
 ```
-  ├── UCI HAR Dataset
-  │	  ├── README.txt
-  │   ├── features_info.txt    Shows information about the variables used on the feature vector.
-  │	  ├── features.txt    List of all features.
-  │	  ├── activity_labels.txt    Links the class labels with their activity name.
-  │   ├── test
-  │   │   │── X_test.txt': Test set.
-  │   │   │── y_test.txt': Test labels.
-  │   │   │── subject_test.txt': Each row identifies the subject (by numeric ID). Range is from 1 to 30.
-  │   └── train
-  │   │   │── X_train.txt': Training set.
-  │   │   │── y_train.txt': Training labels.
-  │   │   │── subject_train.txt': Each row identifies the subject (by numeric ID). Range is from 1 to 30.
+├── UCI HAR Dataset
+	├── README.txt
+	├── features_info.txt    Shows information about the variables used on the feature vector.
+	├── features.txt    List of all features.
+	├── activity_labels.txt    Links the class labels with their activity name.
+	└── test
+		│── X_test.txt': Test set.
+		│── y_test.txt': Test labels.
+		│── subject_test.txt': Each row identifies the subject (by numeric ID). Range is from 1 to 30.
+	└── train
+		│── X_train.txt': Training set.
+		│── y_train.txt': Training labels.
+		│── subject_train.txt': Each row identifies the subject (by numeric ID). Range is from 1 to 30.
   ```
 
 ## Modifications and Transformations
 The `run_analysis.R` script performes the following transformations to the original data set:
-1. The test and train data sets are merged together by row into a single data set.
-2. Two additional factor variables are prepended to the data set: **activity** and **subject**.
-	* activity contains links to the class labels with their lower case activity name. Range is from 1 to 6.
-	* subject is identifies the subject (by numeric ID). Range is from 1 to 30.
-3. The merged data set contains several duplicate variables. Duplicate column variables are removed.
-4. The data set is further subsetted by preserving the activity and subject variables and all columns containing the characters "mean" or "std".
-5. The script then cleans the variable names by removing the symbols and capitalizes the factors.
-6. The tidy data set is created by grouping the data by subject and activity, then taking the average of each variable for each activity and each subject.
-7. The tidy data set with the variable names is written to the working directory as tidy.txt.
+1.The test and train data sets are merged together by row into a single data set.
+2.Two additional factor variables are prepended to the data set: **activity** and **subject**.
+	*activity contains links to the class labels with their lower case activity name. Range is from 1 to 6.
+	*subject is identifies the subject (by numeric ID). Range is from 1 to 30.
+3.The merged data set contains several duplicate variables. Duplicate column variables are removed.
+4.The data set is further subsetted by preserving the activity and subject variables and all columns containing the characters "mean" or "std".
+5.The script then cleans the variable names by removing the symbols and capitalizes the factors.
+6.The tidy data set is created by grouping the data by subject and activity, then taking the average of each variable for each activity and each subject.
+7.The tidy data set with the variable names is written to the working directory as `tidy.txt`.
 
 ## Tidy Data Set Description
-The tidy data set in the `tidy.txt` file contains *X*X*X rows with X*X*X* variables.
+The tidy data set in the `tidy.txt` file contains 180 rows with 60 variables.
--- run_analysis.R
+++ run_analysis.R
@@ -1,72 +1,68 @@
-# This R script was developed for the Coursera Getting and Cleaning Data
-# course.
+# Getting & Cleaning Data
+# Final Assignment
+
+#0. LOAD LIBRARIES, DOWNLOAD DATASET, UNZIP, AND LOAD INTO TABLES #
+library(dplyr)
+
 if(!file.exists("./data")){dir.create("./data")}
 fileUrl <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip"
 download.file(fileUrl, destfile = "./data/UCI_HAR_Dataset.zip", method = "curl")
 unzip("./data/UCI_HAR_Dataset.zip", exdir="./data")
 
-library(dplyr)
-# Read the data into R
-testdata <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
-testActivities <- read.table("./data/UCI HAR Dataset/test/y_test.txt", col.names=c("activity_id"))
-traindata <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
-trainActivities <- read.table("./data/UCI HAR Dataset/train/y_train.txt", col.names=c("activity_id"))
-featureLabels <- read.table("./data/UCI HAR Dataset/features.txt")
-activityLabels <- read.table("./data/UCI HAR Dataset/activity_labels.txt", col.names=c("activity_id","activity"))
-testSubjectId <- read.table("./data/UCI HAR Dataset/test/subject_test.txt", col.names=c("subject_id"))
-trainSubjectId <- read.table("./data/UCI HAR Dataset/train/subject_train.txt", col.names=c("subject_id"))
+testData <- read.table("./data/UCI HAR Dataset/test/X_test.txt")
+testLabels <- read.table("./data/UCI HAR Dataset/test/y_test.txt")
+testSubject <- read.table("./data/UCI HAR Dataset/test/subject_test.txt")
 
-# get indicies of variables with 'mean' or 'std' in the name
-meanStdVariables <- grep("mean|std", featureLabels$V2, value = FALSE)
-# get the names of variables with 'mean' or 'std' in the name
-meanStdVariableNames <- grep("mean|std", featureLabels$V2, value = TRUE)
-# Add data_subset and activity column names
-finalNames <- c(meanStdVariableNames, "subject_id", "data_subset", "activity")
-# convert values to syntactically valid column names
-finalNames <- make.names(finalNames, unique=TRUE)
-# remove numbers and dots from values
-finalNames <- gsub("([0-9])|(\\.)+", '', finalNames)
+trainData <- read.table("./data/UCI HAR Dataset/train/X_train.txt")
+trainLabels <- read.table("./data/UCI HAR Dataset/train/y_train.txt")
+trainSubject <- read.table("./data/UCI HAR Dataset/train/subject_train.txt")
 
-# expand abbreviated column names
-finalNames <- gsub("[Ff]req", 'Frequency', finalNames)
-finalNames <- gsub("^t", 'time', finalNames)
-finalNames <- gsub("^f", 'frequency', finalNames)
-finalNames <- gsub("Acc", 'Acceleration', finalNames)
-finalNames <- gsub("std", 'StandardDeviation', finalNames)
-finalNames <- gsub("[Mm]ag", 'Magnitude', finalNames)
-finalNames <- gsub("mean", 'Mean', finalNames)
+features <- read.table("./data/UCI HAR Dataset/features.txt", header = FALSE, as.is = TRUE, col.names = c("id", "feature"))
+activities <- read.table("./data/UCI HAR Dataset/activity_labels.txt", header = FALSE, col.names = c("id", "activity"))
 
-# extract indicies
-testdata <- testdata[,meanStdVariables]
-traindata <- traindata[,meanStdVariables]
-# add activity_id and data_subset
-testdata <- mutate(testdata, subject_id = testSubjectId$subject_id, activity_id = testActivities$activity_id, data_subset = 'TEST')
-traindata <- mutate(traindata, subject_id = trainSubjectId$subject_id, activity_id = trainActivities$activity_id, data_subset = 'TRAIN')
-# combine test and train datasets
-combined <- bind_rows(testdata, traindata)
-# join the activityLabels to the combined dataset by activity_id columns
-combined_merged <- merge(combined, activityLabels, by="activity_id")
-# remove activity_id column
-final <- select(combined_merged, -activity_id)
-# add names to columns
-names(final) <- finalNames
 
-# Cleanup R environment variables
-rm(activityLabels, testdata, testActivities, testSubjectId, traindata, trainActivities, trainSubjectId, featureLabels, combined, combined_merged, finalNames, meanStdVariables, meanStdVariableNames)
+#1. Merge the training and the test sets to create one data set.
+test <- cbind(testLabels, testSubject, testData)
+train <- cbind(trainLabels, trainSubject, trainData)
+dataset <- rbind(test,train)
+names(dataset) <- c("activity", "subject", as.character(features[,2]))
+dataset$subject <-as.factor(dataset$subject)
+# remove duplicate columns
+dataset <- dataset[, !duplicated(colnames(dataset))]
+# cleanup variable space
+#rm(testData, testLabels, testSubject, trainData, trainLabels, trainSubject, test, train)
 
-# remove data_subset and subject_id columns, group by activity, and compute mean for remaining columns
-activity <- final %>% select(-data_subset, -subject_id) %>% group_by(activity) %>% summarise_each(funs(mean))
-# prepend column names with 'average_' except activity column
-names(activity) <- c(names(activity)[1], paste("average", names(activity)[2:80], sep='_'))
 
-# remove data_subset and activity columns, group by subject identifier, and compute mean for remaining columns
-subject <- final %>% select(-data_subset, -activity) %>% group_by(subject_id) %>% summarise_each(funs(mean))
-# prepend column names with 'average_' except subject_id column
-names(subject) <- c(names(subject)[1], paste("average", names(subject)[2:80], sep='_'))
+#2. Extract only the measurements on the mean and standard deviation for each measurement.
+extractedData <-select(dataset, activity, subject, contains("mean()"), contains("std()"))
 
-# combine datasets
-tidyDataset <- bind_rows(activity, subject)
-# reorder the subject_id column from the end to the second position
-tidyDataset <- tidyDataset[,c(1,81,c(2:80))]
 
-write.table(tidyDataset, file="tidyDataset.table", row.name=FALSE)
+#3. Use descriptive activity names to name the activities in the data set
+activities <- mutate(activities, activity=tolower(activity))
+extractedData$activity[extractedData$activity==1] <- activities$activity[1]
+extractedData$activity[extractedData$activity==2] <- activities$activity[2]
+extractedData$activity[extractedData$activity==3] <- activities$activity[3]
+extractedData$activity[extractedData$activity==4] <- activities$activity[4]
+extractedData$activity[extractedData$activity==5] <- activities$activity[5]
+extractedData$activity[extractedData$activity==6] <- activities$activity[6]
+extractedData$activity <- as.factor(extractedData$activity)
+
+
+#4. Appropriately labels the data set with descriptive variable names.
+names(extractedData) <- gsub("^t", "time", names(extractedData))
+names(extractedData) <- gsub("^f", "frequency", names(extractedData))
+names(extractedData) <- gsub("Acc", "Accelerometer", names(extractedData))
+names(extractedData) <- gsub("[Mm]ag", 'Magnitude', names(extractedData))
+names(extractedData) <- gsub("Gyro", "Gyroscope", names(extractedData))
+names(extractedData) <- gsub('mean', 'Mean', names(extractedData))
+names(extractedData) <- gsub('std', 'Std', names(extractedData))
+names(extractedData) <- gsub("\\-", "\\.", names(extractedData))
+names(extractedData) <- gsub("\\(|\\)", "", names(extractedData))
+names(extractedData) <- gsub("BodyBody", "Body", names(extractedData))
+
+
+#5. From the data set in step 4, creates a second, independent tidy data set with the average of each variable for each activity and each subject.
+tidyDataset <- extractedData %>% group_by(subject, activity) %>%
+    summarise_each(funs(mean(.,na.rm=TRUE)),-subject, -activity)
+
+write.table(tidyDataset, file="tidy.txt", row.name=FALSE)
